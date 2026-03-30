c708decbaea7104b33fe92010821c0c246f2f48b
Abu10thahir7-github <abu10thahir7@gmail.com>
2026-03-30 20:51:31 +0530

    feat: add Navbar component with GSAP animations and responsive design

---
+++ constants/index.jsx
@@ -0,0 +1,6 @@
+export const navbarLinks = [
+  { title: "Cocktail", id: "cocktail" },
+  { title: "Home", id: "home" },
+  { title: "About", id: "about" },
+  { title: "Contact", id: "contact" },
+];
